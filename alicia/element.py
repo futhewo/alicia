686b03bf96ede875c1414091051ee00059fc1376
fuzzTheWorld <fuzztheworld@gmail.com>
2017-06-26 13:44:12 +0200

    Refacto the element object.
    In order to simplify the API and clarify the code.

--- alicia/element.py
+++ alicia/element.py
@@ -25,10 +25,6 @@
 
 
 # Imports #####################################################################
-import random
-import string
-import copy
-
 from alicia.configuration import *
 from alicia.utils import *
 
@@ -42,62 +38,50 @@ class Element(object):
     """
 
     # Constructor =========================================
-    def __init__(self, name=None, fuzzing=None, overflowing=None, randoming=None, fuzzingNumber=None, overflowNumber=None, randomRate=None, randomNumber=None):
+    def __init__(self, name=None, static=False, weight=1.0):
+        """
+            @param (string)name: name of the element.
+            @param (bool)static: can be modified.
+            @param (float)weight: ponderation for the number of fuzz-cases.
+        """
 
+        # Identification
         global elementId
         self.elementId = elementId
         elementId += 1
         self.type = "Element"
+        self.setName(name)
 
-        self.name = name
-        if name is None:
-            self.name = "Element {0}".format(self.elementId)
+        # Attribute setting
+        assert(type(static) is bool)
+        assert(type(weight) is float)
+        self.static = static
+        self.weight = weight
 
         # Bound fields
         self.boundElements = []   # Fields that depend on this field.
         self.notifiable = True
 
-        # Getting the default attributes in the current configuration object if they are not defined.
-        if fuzzing is None:
-            fuzzing = conf.fuzzing
-        if overflowing is None:
-            overflowing = conf.overflowing
-        if randoming is None:
-            randoming = conf.randoming
-        if fuzzingNumber is None:
-            fuzzingNumber = conf.fuzzingNumber
-        if overflowNumber is None:
-            overflowNumber = conf.overflowNumber
-        if randomRate is None:
-            randomRate = conf.randomRate
-        if randomNumber is None:
-            randomNumber = conf.randomNumber
-      
-        # Configuration attributes 
-        self.fuzzing        = fuzzing
-        self.overflowing    = overflowing
-        self.randoming      = randoming
-        self.fuzzingNumber  = fuzzingNumber 
-        self.overflowNumber = overflowNumber
-        self.randomRate     = randomRate
-        self.randomNumber   = randomNumber
-
-        # Control on random rate value
-        if randomRate > 100:
-            self.randomRate = 100
-
+        # Parsing
         self.parsed = False
 
+        self.preForecast()
 
 
     # Actioners ===========================================
+    def setName(self, name):
+        self.name = name
+        if name is None:
+            self.name = "{0} {1}".format(self.type, self.elementId)
+        
+
     # Built-ins ===========================================
     def __str__(self):
         """
             Return a string representing the element.
             @return (string)representation
         """
-        string = "[{0}]".format(self.name)
+        string = "[{0}]\n".format(self.name)
         return string
 
 
@@ -121,73 +105,32 @@ class Element(object):
 
 
     # Fuzzing =============================================
+    def preForecast(self):
+        """
+            Prepare the forecasts. 
+        """
+        self.fuzzNumber = 0
+        self.overflowNumber = 0
+
+
     def forecast(self):
         """
-            Steps are a way to identify each fuzz case. 
-            For understanding, the configuration will give a number of fuzz-cases, which are dubbed 'steps'.
-            Compute the number of fuzz-cases (steps) this element can produce according to the configuration.
+            Return the number of fuzz-case it can produce for this element.
         """
-        return self.totalStepsNumber
+        return self.fuzzNumber
 
 
-    def makeSteps(self):
+    def forecastOverflow(self):
         """
-            Make the computation for steps 
+            Return the number of overflow-case it can produce for this element.
         """
-        self.stepsNumber = 1        # Default step
-        self.totalStepsNumber = 1   # Including the subElementSteps
-
-        # Make a table with the steps for each type of fuzzing.
-        self.steps = [1, 0, 0, 0]
-
-    
-    def getFuzzTypeAfterSteps(self, steps):
-        """
-            Return the fuzz type in which the steps lead, and the steps remaining.
-            @param (int)steps
-            @return ((int)fuzz type, (int)remaining steps)
-        """
-        remainingSteps = steps
-        assert(self.totalStepsNumber > remainingSteps >= 0)
-
-        fuzzType = 0
-        while remainingSteps >= self.steps[fuzzType]:
-            remainingSteps -= self.steps[fuzzType]
-            fuzzType += 1
-        return (fuzzType, remainingSteps)
-
+        return self.overflowNumber
+        
 
     def fuzz(self, steps):
         """
-            Fuzz the element. The element is then modified.
-            For an artificial element, it does nothing though.
-            @param (int)steps
-        """
-        pass
-        
-
-    def compose(self):
-        """
-            Compose a protocol and returns the list of all its elements' subvalues.
-            For an artificial element, which has neither value, nor son, it returns nothing. 
-            @return (string or [string] or [[string]])
-        """
-        return []
-
-
-    def nope(self, steps):
-        """
-            Return a standard value.
-            @param (int)steps: the standard value reference
-        """
-        pass
-
-
-    def basic(self, steps, singleChar=False):
-        """
             Basic fuzzing.
             @param (int)steps: the fuzz-case reference
-            @param (bool)singleChar: tells if there is only one character that can take the final value (for instance paddingField)
         """
         pass
 
@@ -200,16 +143,23 @@ class Element(object):
         pass
 
 
-    def random(self, steps):
+    def compose(self):
         """
-            Strictly random fuzzing.
+            Make a nested list of subElement's value.
+            @return (string or [string] or [[string]])
         """
-        pass
+        return []
 
+
+    def commit(self):
+        """
+            Set the working value as current value.
+            Recursive.
+        """
 
     def clean(self):
         """
-            Set all subElements to their default value.
+            Set the default value as current value.
             Recursive.
         """
         pass
@@ -221,18 +171,4 @@ class Element(object):
             Try to parse a value into the element according to its structure.
         """
         pass
-
-
-    def parseClean(self):
-        pass
-
-
-# ElementsBlock ###############################################################
-class ElementsBlock():
-    def __init__(self, value, elements):
-        self.value = value
-        self.elements = elements
-
-
-
 
---
+++ tests/test_element.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Unit-tests of Alicia
+
+
+
+# Imports #####################################################################
+from nose.tools import *
+
+from alicia.configuration import *
+from alicia.element import *
+from alicia.utils import *
+
+
+
+# Nose functions ##############################################################
+def setup():
+    pass
+
+def teardown():
+    pass
+
+
+
+# Element #####################################################################
+def test_Element___init__():
+    global elementId
+    elementId = 0
+    element0 = Element()
+    assert_equals(element0.elementId        , 0)
+    assert_equals(element0.type             , "Element")
+    assert_equals(element0.name             , "Element 0")
+    assert_equals(element0.static           , False)
+    assert_equals(element0.weight           , 1.0)
+    assert_equals(element0.boundElements    , [])
+    assert_equals(element0.notifiable       , True)
+    assert_equals(element0.parsed           , False)
+
+    element1 = Element(name="My Element", static=True, weight=2.0)
+    assert_equals(element1.elementId        , 1)
+    assert_equals(element1.type             , "Element")
+    assert_equals(element1.name             , "My Element")
+    assert_equals(element1.static           , True)
+    assert_equals(element1.weight           , 2.0)
+    assert_equals(element1.boundElements    , [])
+    assert_equals(element1.notifiable       , True)
+    assert_equals(element1.parsed           , False)
+
+# setName already tested.
+
+def test_element___str__():
+    element0 = Element("My Element")
+    assert_equals(str(element0)             , "[My Element]\n") 
+
+
+# update will be tested with more complete models. 
+
+def test_element_forecast():
+    element0 = Element()
+    assert_equals(element0.fuzzNumber       , 0)
+    assert_equals(element0.overflowNumber   , 0)
+    assert_equals(element0.forecast()       , 0)
+    assert_equals(element0.forecastOverflow(), 0)
+    # To be retested for each subElements.
+
+
