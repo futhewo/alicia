fe2ed36f675aaf5acb3b8d3dbe7c907386f6fd85
fuzzTheWorld <fuzztheworld@gmail.com>
2017-06-29 08:22:40 +0200

    Add the global content class
    This class defines the content (type and constraints of a field).

---
+++ alicia/content.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Vocabulary structure, i.e. definition of fields and the structure that comes with it.
+
+
+# Imports #####################################################################
+
+
+
+# Content #####################################################################
+class Content(object):
+    """
+        Defines constraints on the content of a field.
+    """
+
+    # Constructor =========================================
+    def __init__(self, value):
+        self.type = "Content"
+
+        self.default = value
+        self.current = value
+        self.future = value
+
+
+    # Actioners ===========================================
+    def update(self, value):
+        self.current = value
+
+
+    def compose(self):
+        return self.current
+
+
+    def commit(self):
+        self.current = self.future
+
+
+    def clean(self):
+        self.current = self.default
+
+
+    # Fuzzing =============================================
+    def newCharacter(self, rand):
+        """
+            Return a new character concording with this type.
+        """
+        return chr(rand.randint(0, 255))
+
+
+    def fuzz(self, minSize, maxSize, rand, steps):
+        """
+            Fuzz the given value that needs to stay in the size range.
+            @param (string)value: the value to be fuzzed
+            @param (int)minSize
+            @param (int)maxSize
+            @param (random.Random)rand
+        """
+        value = ""
+        for i in range(rand.randint(minSize, maxSize)):
+            value += self.newCharacter(rand)
+        self.update(value)
+
---
+++ tests/test_content.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Unit-tests of Alicia
+
+
+
+# Imports #####################################################################
+from nose.tools import *
+import random
+
+from alicia.content import *
+from alicia.configuration import *
+from alicia.utils import *
+
+
+
+# Nose functions ##############################################################
+def setup():
+    pass
+
+def teardown():
+    pass
+
+
+
+# Content #####################################################################
+def test_Content___init__():
+    content0 = Content("ABCDE")
+    assert_equals(content0.type             , "Content")
+    assert_equals(content0.default          , "ABCDE")
+    assert_equals(content0.current          , "ABCDE")
+    assert_equals(content0.future           , "ABCDE")
+
+
+def test_Content_update():
+    content0 = Content("ABCDE")
+    content0.update("FGHIJ")
+    assert_equals(content0.current          , "FGHIJ")
+    
+
+def test_Content_compose():
+    content0 = Content("ABCDE")
+    assert_equals(content0.compose()        , "ABCDE")
+
+
+def test_Content_commit():
+    content0 = Content("ABCDE")
+    content0.future = "FGHIJ"
+    content0.commit()
+    assert_equals(content0.current          , "FGHIJ")
+
+
+def test_Content_clean():
+    content0 = Content("ABCDE")
+    content0.update("FGHIJ")
+    content0.clean()
+    assert_equals(content0.current          , "ABCDE")
+
+
+def test_Content_newCharacter():
+    content0 = Content("ABCDE")
+    rand = random.Random(0)
+    assert_equals(content0.newCharacter(rand), "\xd8")
+
+
+def test_Content_compose():
+    content0 = Content("ABCDE")
+    rand = random.Random(0)
+    content0.fuzz(5, 5, rand, 0)
+    assert_equals(content0.current           , "\xc2kB\x82g")
+
