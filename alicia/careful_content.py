a042f47428f7d82094e0ec89f792bc3c00ec9f53
fuzzTheWorld <fuzztheworld@gmail.com>
2017-06-29 08:49:01 +0200

    Add the CarefulContent object
    It is a Content object that will manipulate values as close as the default value he was given.
    In opposition, the usual "Content" obect use totally random values.

---
+++ alicia/careful_content.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Vocabulary structure, i.e. definition of fields and the structure that comes with it.
+
+
+# Imports #####################################################################
+from content import *
+
+
+
+# CarefulContent ##############################################################
+class CarefulContent(Content):
+    """
+        Defines constraints on the content of a field.
+        Will use values to look like the default value.
+    """
+
+    # Constructor =========================================
+    def __init__(self, value):
+        Content.__init__(self, value)
+        self.type = "CarefulContent"
+
+
+    # Actioners ===========================================
+    # Fuzzing =============================================
+    def newCharacter(self, rand):
+        """
+            Return a new character concording with this type.
+        """
+        return rand.choice(list(self.default))
+
+
+    def fuzz(self, minSize, maxSize, rand, steps):
+        """
+            Fuzz the given value that needs to stay in the size range.
+            @param (string)value: the value to be fuzzed
+            @param (int)minSize
+            @param (int)maxSize
+            @param (random.Random)rand
+        """
+        value = ""
+        for i in range(rand.randint(minSize, maxSize)):
+            value += self.newCharacter(rand)
+        self.update(value)
+
---
+++ tests/test_careful_content.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Unit-tests of Alicia
+
+
+
+# Imports #####################################################################
+from nose.tools import *
+import random
+
+from alicia.careful_content import *
+from alicia.configuration import *
+from alicia.utils import *
+
+
+
+# Nose functions ##############################################################
+def setup():
+    pass
+
+def teardown():
+    pass
+
+
+
+# CarefulContent ##############################################################
+def test_CarefulContent___init__():
+    content0 = CarefulContent("ABCDE")
+    assert_equals(content0.type             , "CarefulContent")
+    assert_equals(content0.default          , "ABCDE")
+    assert_equals(content0.current          , "ABCDE")
+    assert_equals(content0.future           , "ABCDE")
+
+
+def test_CarefulContent_newCharacter():
+    content0 = CarefulContent("ABCDE")
+    rand = random.Random(0)
+    assert_equals(content0.newCharacter(rand), "E")
+
+
+def test_CarefulContent_compose():
+    content0 = CarefulContent("ABCDE")
+    rand = random.Random(0)
+    content0.fuzz(5, 5, rand, 0)
+    assert_equals(content0.current           , "DCBCC")
+
