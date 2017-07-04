945d7304dd5b84dc3d3c57060a8911b248b4b225
fuzzTheWorld <fuzztheworld@gmail.com>
2017-07-04 19:37:22 +0200

    Add BinaryContent
    Represent a bitarray or flags.

---
+++ alicia/binary_content.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Vocabulary structure, i.e. definition of fields and the structure that comes with it.
+
+
+# Imports #####################################################################
+import bitarray
+
+from configuration import *
+from content import *
+from utils import *
+
+
+# BinaryContent ###############################################################
+class BinaryContent(Content):
+    """
+        Defines constraints on the content of a field.
+        Values are bitarrays.
+    """
+
+    # Constructor =========================================
+    def __init__(self, value):
+        Content.__init__(self, value)
+        self.type = "BinaryContent"
+        self.binaryCurrent = bitarray.bitarray()
+        self.binaryCurrent.frombytes(value)
+
+    # Actioners ===========================================
+    def update(self, value):
+        self.current = value
+        self.binaryCurrent = bitarray.bitarray()
+        self.binaryCurrent.frombytes(value)
+
+
+    def binaryUpdate(self, value):
+        self.binaryCurrent = value
+        self.current = self.binaryCurrent.tobytes()
+
+
+    # Fuzzing =============================================
+    def newCharacter(self, rand):
+        """
+            Return a new character concording with this type.
+        """
+        return chr(rand.randint(0, 255))
+
+
+    def fuzz(self, minSize, maxSize, rand, steps):
+        """
+            Fuzz the given value that needs to stay in the size range.
+            @param (int)minSize
+            @param (int)maxSize
+            @param (random.Random)rand
+            @param (int)steps
+        """
+        # Sequential bit flip
+        if steps < len(self.binaryCurrent):
+            # Bit flip
+            binaryValue = self.binaryCurrent
+            binaryValue[steps] ^= True
+            self.binaryUpdate(binaryValue)
+
+        # Random bit flips
+        else:
+            indexes = generateIndexes(len(self.binaryCurrent), rand, configuration.randomness)
+            binaryValue = self.binaryCurrent
+            for index in indexes:
+                binaryValue[index] ^= True
+            self.binaryUpdate(binaryValue)
+
---
+++ tests/test_binary_content.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Unit-tests of Alicia
+
+
+
+# Imports #####################################################################
+from nose.tools import *
+import random
+
+from alicia.binary_content import *
+
+
+
+# Nose functions ##############################################################
+def setup():
+    pass
+
+def teardown():
+    pass
+
+
+
+# BinaryContent ###############################################################
+def test_BinaryContent___init__():
+    content0 = BinaryContent("\xff\x00")
+    assert_equals(content0.type             , "BinaryContent")
+    assert_equals(content0.default          , "\xff\x00")
+    assert_equals(content0.current          , "\xff\x00")
+    assert_equals(content0.future           , "\xff\x00")
+    assert_equals(content0.binaryCurrent.tobytes(), "\xff\x00")
+    
+
+def test_BinaryContent_update():
+    content0 = BinaryContent("\xff\x00")
+    content0.update("\x00\xff")
+    assert_equals(content0.current          , "\x00\xff")
+    assert_equals(content0.binaryCurrent.tobytes(), "\x00\xff")
+
+
+def test_BinaryContent_binaryUpdate():
+    content0 = BinaryContent("\xff\x00")
+    content0.binaryUpdate(bitarray.bitarray("0000000011111111"))
+    assert_equals(content0.current          , "\x00\xff")
+    assert_equals(content0.binaryCurrent.tobytes(), "\x00\xff")
+
+
+def test_BinaryContent_commit():
+    content0 = BinaryContent("\xff\x00")
+    content0.future = "\x00\xff"
+    content0.commit()
+    assert_equals(content0.current          , "\x00\xff")
+    assert_equals(content0.binaryCurrent.tobytes(), "\x00\xff")
+
+
+def test_BinaryContent_clean():
+    content0 = BinaryContent("\x00\xff")
+    content0.update("\xff\x00")
+    content0.clean()
+    assert_equals(content0.current          , "\x00\xff")
+    assert_equals(content0.binaryCurrent.tobytes(), "\x00\xff")
+
+
+def test_BinaryContent_newCharacter():
+    content0 = BinaryContent("\x00\xff")
+    rand = random.Random(0)
+    assert_equals(content0.newCharacter(rand), "\xd8")
+
+    rand = random.Random(1)
+    assert_equals(content0.newCharacter(rand), '"')
+
+
+def test_BinaryContent_fuzz():
+    content0 = BinaryContent("\x00\xff")
+    rand = random.Random(1)
+    content0.fuzz(2, 2, rand, 0)
+    assert_equals(content0.current           , "\x80\xff")
+
+    content0.clean()
+    rand = random.Random(2)
+    content0.fuzz(2, 2, rand, 15)
+    assert_equals(content0.current           , "\x00\xfe")
+
+
+    content0.clean()
+    rand = random.Random(1)
+    content0.fuzz(2, 2, rand, 16)
+    assert_equals(content0.current           , "\x80;")
+
