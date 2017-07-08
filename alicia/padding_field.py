22eb99a46b8e64a8fad9735991aa8e7ff8e3c851
fuzzTheWorld <fuzztheworld@gmail.com>
2017-07-08 11:34:44 +0200

    Add the PaddingField.
    It pads a list of elements in order to the whole pack to have a predefinite size.

--- alicia/padding_field.py
+++ alicia/padding_field.py
@@ -29,38 +29,32 @@ import random
 import string
 import copy
 
-from alicia.field import *
+from alicia.open_field import *
 from alicia.utils import *
 
 
 
 # PaddingField ############################################
-class PaddingField(Field):
+class PaddingField(OpenField):
     """
         A field to make padding to a specific paddingBlockSize with specific occurences of paddingElement.
     """
 
     # Constructor =========================================
-    def __init__(self, paddingElement, paddingBlockSize, fields, name=None, fuzzing=None, overflowing=None, randoming=None, fuzzingNumber=None, overflowNumber=None, randomRate=None, randomNumber=None):
-        Field.__init__(self, "", 0, 0, [], name, fuzzing, overflowing, randoming, fuzzingNumber, overflowNumber, randomRate, randomNumber)
+    def __init__(self, content, paddingSize, elements, name=None, weight=1.0):
+        OpenField.__init__(self, content, paddingSize, 0, name, weight)
         
-        if name is None:
-            self.name = "PaddingField {0}".format(self.elementId)
         self.type = "PaddingField"
+        self.setName(name)
 
-        self.paddingElement = paddingElement     # Usual value
-        self.paddingBlockSize = paddingBlockSize # Targeted size for the padding
-        
         # Covered fields
-        self.fields = fields
-        for field in self.fields:
-            field.boundElements.append(self)
-        
-        # Generate a clever default value
-        self.default = self.paddingElement * self.computeSize()
-        self.value  = self.default
-        self.values = [self.value]
+        self.elements = elements
+        for element in self.elements:
+            element.boundElements.append(self)
+
+        self.padder = content.default[0]
         self.notify()
+        self.content.default = self.content.current # Save the default size.
 
         self.remainingSize = 0
 
@@ -72,11 +66,11 @@ class PaddingField(Field):
             Return a string representing the element.
             @return (string)representation
         """
-        string = "[{0}]: ({1}:{2}) [".format(self.name, self.paddingElement, self.paddingBlockSize)
+        string = "[{0}]: ({1}:{2}) [".format(self.name, self.padder, self.maxSize)
         nameList = []
-        for field in self.fields:
-            nameList.append(field.name)
-        string += ", ".join(nameList) + "]"
+        for element in self.elements:
+            nameList.append(element.name)
+        string += ", ".join(nameList) + "]\n"
         return string
 
 
@@ -84,10 +78,10 @@ class PaddingField(Field):
     def notify(self):
         """
             Called when the base field is modified in order to update the size.
-            Its value is stuffed with paddingElement in order that its value concatenated to the value of the base field have a size multiple of paddingBlockSize.
+            Its value is stuffed with padder in order that its value concatenated to the value of the base field have a size multiple of the padding size.
         """
         if self.notifiable:
-            self.update(self.paddingElement * self.computeSize())
+            self.update(self.padder * self.computeSize())
 
 
     def computeSize(self):
@@ -95,81 +89,13 @@ class PaddingField(Field):
             Add the sizes of all the covered fields and deduce the needed size.
         """
         size = 0
-        for field in self.fields:
-            size += field.getLength()
-        return self.paddingBlockSize - (size % self.paddingBlockSize)
+        for element in self.elements:
+            size += element.getSize()
+        return self.maxSize - (size % self.maxSize)
 
 
     # Fuzzing =============================================
-    def nope(self, steps):
-        """
-            Return a standard value.
-            @param (int)steps: the standard value reference
-        """
-        debug(">[{0}] {1} (F): Noping.".format(steps, self.name), conf.verbose)
-        self.update(self.paddingElement * self.computeSize())
-
-
-    def basic(self, steps, singleChar=False):
-        """
-            @param (int)steps
-        """
-        self.notifiable = False
-        Field.basic(self, steps, singleChar=True)
-
-
-    def overflow(self, steps):
-        """
-            Add a lot of padding elements while respecting the padding block size.
-            @param (int)steps
-        """
-        self.notifiable = False
-        r = random.Random(steps)
-        numberOfTimes = r.randint(conf.overflowMin * (steps + 1), conf.overflowMax * (steps + 1)) ** conf.agressivity
-        numberOfTimes += self.paddingBlockSize - numberOfTimes % self.paddingBlockSize
-        debug(">[{0}] {1} (F): Overflow by {2} {3}".format(steps, self.name, numberOfTimes, repr(self.paddingElement)), conf.verbose)
-        self.update(self.paddingElement * numberOfTimes)
-
-
-    def random(self, steps):
-        """
-            Stuff the padding size with one repeated random char.
-            @param (int)steps
-        """
-        self.notifiable = False
-
-        r = random.Random(steps)
-        string = chr(r.randint(0, 255)) * len(self.value)
-        debug(">[{0}] {1} (F): Random".format(steps, self.name), conf.verbose)
-        self.update(string)
-
-
-
     # Parsing =============================================
     def parse(self, value, backward=False, root=False):
-        retValue = value
-        eatenValue = ""
-        if backward:
-            retList = list(retValue)
-            retList.reverse()
-            retValue = ''.join(retList)
-            while retValue.find(self.paddingElement) == 0:
-                retValue = retValue[len(self.paddingElement):]
-                eatenValue += self.paddingElement
-            retList = list(retValue)
-            retList.reverse()
-            retValue = ''.join(retList)
-
-        else:
-            while retValue.find(self.paddingElement) == 0:
-                retValue = retValue[len(self.paddingElement):]
-                eatenValue += self.paddingElement
-
-        self.value = eatenValue
-        #TODO: check that the size is respected.
-        self.parsed = True
-
-        if root:
-            assert(len(retValue) == 0)
-        return retValue
+        pass
 
---
+++ tests/test_padding_field.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Unit-tests of Alicia
+
+
+
+# Imports #####################################################################
+from nose.tools import *
+
+from alicia.string_content import *
+from alicia.integer_content import *
+from alicia.static_field import *
+from alicia.padding_field import *
+from alicia.node import *
+from alicia.utils import *
+
+
+
+# Nose functions ##############################################################
+def setup():
+    pass
+
+def teardown():
+    pass
+
+
+# PaddingField ################################################################
+def test_PaddingField___init__():
+    global elementId
+    elementId = 0
+    content0 = StringContent("#")
+    element0 = PaddingField(content0, 32, [])
+    assert_equals(element0.elementId        , 0)
+    assert_equals(element0.type             , "PaddingField")
+    assert_equals(element0.name             , "PaddingField 0")
+    assert_equals(element0.static           , False)
+    assert_equals(element0.weight           , 1.0)
+    assert_equals(element0.boundElements    , [])
+    assert_equals(element0.notifiable       , True)
+    assert_equals(element0.parsed           , False)
+    assert_equals(element0.content          , content0)
+    assert_equals(element0.padder           , "#")
+    assert_equals(element0.maxSize          , 32)
+    assert_equals(element0.elements         , [])
+
+    content1 = IntegerContent(12345)
+    element1 = StaticField(content1, name="My StaticField")
+    element2 = PaddingField(content0, 24, [element1], name="My PaddingField", weight=2.5)
+    assert_equals(element2.elementId        , 2)
+    assert_equals(element2.type             , "PaddingField")
+    assert_equals(element2.name             , "My PaddingField")
+    assert_equals(element2.static           , False)
+    assert_equals(element2.weight           , 2.5)
+    assert_equals(element2.boundElements    , [])
+    assert_equals(element2.notifiable       , True)
+    assert_equals(element2.parsed           , False)
+    assert_equals(element2.content          , content0)
+    assert_equals(element2.padder           , "#")
+    assert_equals(element2.maxSize          , 24)
+    assert_equals(element2.elements         , [element1])
+    assert_equals(content0.compose()        , "#" * 20)
+    assert_equals(element1.boundElements    , [element2])
+
+
+def test_PaddingField___str__():
+    content0 = StringContent("#")
+    content1 = IntegerContent(12345)
+    element0 = StaticField(content1, name="My StaticField")
+    element1 = PaddingField(content0, 24, [element0], name="My PaddingField")
+    assert_equals(str(element1)             , "[My PaddingField]: (#:24) [My StaticField]\n")
+
+
+def test_PaddingField_notify():
+    content0 = StringContent("#")
+    content1 = IntegerContent(12345)
+    element0 = StaticField(content1)
+    element1 = PaddingField(content0, 8, [element0])
+    element1.notify()
+    assert_equals(element1.compose()       , "####")
+
+    # Harder example
+    content2 = StringContent("ABCDEF")
+    content3 = IntegerContent(32)
+    element2 = CloseField(content2)
+    element3 = CloseField(content3)
+    node0 = Node([element2])
+    node1 = Node([node0, element3])
+    node2 = Node([element0, element1])
+    node3 = Node([node1, node2])
+
+    content4 = StringContent("#")
+    element4 = PaddingField(content4, 8, [node3])
+    element4.notify()
+    assert_equals(element4.compose()       , "######")
+
+
+def test_PaddingField_computeSize():
+    content0 = StringContent("#")
+    content1 = IntegerContent(12345)
+    element0 = StaticField(content1)
+    element1 = PaddingField(content0, 8, [element0])
+    assert_equals(element1.computeSize()   , 4) 
+
+
+    # Harder example
+    content2 = StringContent("ABCDEF")
+    content3 = IntegerContent(32)
+    element2 = CloseField(content2)
+    element3 = CloseField(content3)
+    node0 = Node([element2])
+    node1 = Node([node0, element3])
+    node2 = Node([element0, element1])
+    node3 = Node([node1, node2])
+
+    content4 = StringContent("#")
+    element4 = PaddingField(content4, 8, [node3])
+    element4.notify()
+    assert_equals(element4.computeSize()       , 6)
+
