3a7cc7edb23975e9a1ceac2f347127930a3df1b6
fuzzTheWorld <fuzztheworld@gmail.com>
2017-07-19 08:13:14 +0200

    Refacto SizeField

--- alicia/size_field.py
+++ alicia/size_field.py
@@ -24,44 +24,40 @@
 # Vocabulary structure, i.e. definition of fields and the structure that comes with it.
 
 
-
 # Imports #####################################################################
 import random
 import string
 import copy
 
-from alicia.integer_field import *
+from alicia.open_field import *
 from alicia.utils import *
 
 
 
 # SizeField ###################################################################
-class SizeField(IntegerField):
+class SizeField(CloseField):
     """
-        A field that contains the size of a list of fields.
-        Its value is updated after the targeted fields' value.
+        A field that contains the size of a list of elements.
+        Its value is updated after the targeted elements' value.
     """
 
     # Constructor =========================================
-    def __init__(self, fields, size, selfSize=False, endianness="be", minValue=0, maxValue=0, name=None, fuzzing=None, overflowing=None, randoming=None, fuzzingNumber=None, overflowNumber=None, randomRate=None, randomNumber=None):
-        IntegerField.__init__(self, 0, size, endianness, False, minValue, maxValue, [], name, fuzzing, overflowing, randoming, fuzzingNumber, overflowNumber, randomRate, randomNumber)
+    def __init__(self, content, elements, selfSize=False, name=None, weight=1.0):
+        assert(type(content) is IntegerContent)
+        CloseField.__init__(self, content, name, weight)
         
-        if name is None:
-            self.name = "SizeField {0}".format(self.elementId)
         self.type = "SizeField"
-        
-        # Covered fields.
-        self.fields = fields
-        for field in self.fields:
-            field.boundElements.append(self)
+        self.setName(name)
 
-        # Other parameters
         self.selfSize = selfSize
-        self.default = i2sbs(self.computeSize(), self.minSize, self.endianness)
-        self.value = self.default
 
-        # Computation parameters
-        self.remainingSize = 0
+        # Covered fields
+        self.elements = elements
+        for element in self.elements:
+            element.bound(self)
+
+        self.notify()
+        self.content.default = self.content.current # Save the default size.
 
 
     # Actioners ===========================================
@@ -71,109 +67,37 @@ class SizeField(IntegerField):
             Return a string representing the element.
             @return (string)representation
         """
-        string = "[{0}]: ({1}:{2}) [".format(self.name, str(self.minSize), str(self.selfSize))
+        string = "[{0}]: ({1}:{2}) [".format(self.name, self.selfSize, self.content.size)
         nameList = []
-        for field in self.fields:
-            nameList.append(field.name)
-        string += ", ".join(nameList) + "]"
+        for element in self.elements:
+            nameList.append(element.name)
+        string += ", ".join(nameList) + "]\n"
         return string
-        
+
 
     # Notification ========================================
     def notify(self):
         """
             Update its value with the recomputed size.
         """
-        if self.notifiable:
-            self.update(i2sbs(self.computeSize(), self.minSize, self.endianness))
+        self.content.integerUpdate(self.computeSize())
+        self.pushNotification()
 
 
     def computeSize(self):
         """
-            Adds the sizes of all the covered fields.
+            Add the sizes of all the covered fields and deduce the needed size.
         """
         size = 0
-        for field in self.fields:
-            size += field.getLength()
-        # selfSize, does not manage maxSize yet
+        for element in self.elements:
+            size += element.getSize()
         if self.selfSize:
-            size += self.minSize
+            size += self.getSize()
         return size
 
 
     # Fuzzing =============================================
-    def nope(self, steps):
-        """
-            Return a standard value.
-            @param (int)steps: the standard value reference
-        """
-        debug(">[{0}] {1} (F): Noping.".format(steps, self.name), conf.verbose)
-        self.update(i2sbs(self.computeSize(), self.minSize, self.endianness))
-
-
-    def basic(self, steps, singleChar=False):
-        """
-            Modifies the size value, while maintaining her in its bounds, without modifying the real size. It targets the bad managed size.
-            @param (int)steps
-        """
-        self.notifiable = False
-        IntegerField.basic(self, steps)
-
-
-    def overflow(self, steps):
-        """
-            Try the biggest sizes.
-            @param (int)steps
-        """
-        self.notifiable = False
-        IntegerField.overflow(self, steps)
-
-    def random(self, step):
-        """
-            Stuff the size with random chars.
-            @param (int)steps
-        """
-        self.notifiable = False
-        IntegerField.random(self, steps)
-
-
     # Parsing =============================================
     def parse(self, value, backward=False, root=False):
-        self.parsed = True
-        finalValue = ""
-        assert(self.minSize == self.maxSize)
-        eatingSize = self.minSize
-
-        for boundElement in self.boundElements:
-            boundElement.remainingSize -= eatingSize
-
-        assert(len(value) >= eatingSize)
-        if backward:
-            # Eat at the end
-            finalValue = value[:-eatingSize]
-            self.value = value[-eatingSize:]
-        else:
-            # Eat at the beginning
-            finalValue = value[eatingSize:]
-            self.value = value[:eatingSize]
-
-        self.remainingSize += bs2ui(self.value)
-        if self.selfSize: # Remove its own size.
-            self.remainingSize -= self.minSize
-
-        # Keep some space for the known fixed field.
-        for field in self.fields:
-            if field.minSize == field.maxSize:
-                self.remainingSize -= field.minSize
-
-        assert(self.remainingSize >= 0)
-
-        if root:
-            assert(len(finalValue) == 0)
-        return finalValue
-
-
-    def parseClean(self):
-        self.parsed = False
-        self.remainingSize = 0        
+        pass
 
---
+++ tests/test_size_field.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+# -*-  encoding: iso-8859-1 -*-
+
+
+
+###############################################################################
+# Copyright 2017 @fuzztheworld
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+###############################################################################
+# Unit-tests of Alicia
+
+
+
+# Imports #####################################################################
+from nose.tools import *
+
+from alicia.string_content import *
+from alicia.integer_content import *
+from alicia.static_field import *
+from alicia.size_field import *
+from alicia.padding_field import *
+from alicia.node import *
+from alicia.utils import *
+
+
+
+# Nose functions ##############################################################
+def setup():
+    pass
+
+def teardown():
+    pass
+
+
+# SizeField ###################################################################
+def test_SizeField___init__():
+    content0 = IntegerContent(10)
+    element0 = SizeField(content0, [])
+    assert_equals(element0.type             , "SizeField")
+    assert_equals(element0.static           , False)
+    assert_equals(element0.weight           , 1.0)
+    assert_equals(element0.boundElements    , [])
+    assert_equals(element0.notifiable       , True)
+    assert_equals(element0.parsed           , False)
+    assert_equals(element0.content          , content0)
+    assert_equals(element0.selfSize         , False)
+    assert_equals(element0.elements         , [])
+    assert_equals(content0.compose()        , "\x00\x00\x00\x00")
+
+    content1 = StringContent("12345")
+    element1 = OpenField(content1, 32, name="My ContentField")
+    content2 = IntegerContent(0)
+    element2 = SizeField(content2, [element1], selfSize=True, name="My SizeField", weight=2.5)
+    assert_equals(element2.type             , "SizeField")
+    assert_equals(element2.name             , "My SizeField")
+    assert_equals(element2.static           , False)
+    assert_equals(element2.weight           , 2.5)
+    assert_equals(element2.boundElements    , [])
+    assert_equals(element2.notifiable       , True)
+    assert_equals(element2.parsed           , False)
+    assert_equals(element2.content          , content2)
+    assert_equals(element2.selfSize         , True)
+    assert_equals(element2.elements         , [element1])
+    assert_equals(content2.compose()        , "\x00\x00\x00\x09")
+    assert_equals(element1.boundElements    , [element2])
+
+
+def test_SizeField___str__():
+    content0 = StringContent("ABCDE")
+    content1 = IntegerContent(0)
+    element0 = StaticField(content0, name="My StaticField")
+    element1 = SizeField(content1, [element0], name="My SizeField")
+    assert_equals(str(element1)             , "[My SizeField]: (False:4) [My StaticField]\n")
+
+
+def test_SizeField_notify():
+    content0 = StringContent("ABCDE")
+    element0 = OpenField(content0, 32)
+
+    content1 = IntegerContent(0)
+    element1 = SizeField(content1, [element0])
+    element1.notify()
+    assert_equals(element1.compose()       , "\x00\x00\x00\x05")
+
+    # Harder example
+    content2 = StringContent("ABCDEFGHIJKLM")
+    element2 = OpenField(content2, 32)
+    content3 = StringContent("12345")
+    element3 = OpenField(content3, 32)
+    content4 = StringContent("#")
+    element4 = PaddingField(content4, 8, [element0])
+    node0 = Node([element2])
+    node1 = Node([node0, element3])
+    node2 = Node([element0, element4])
+    node3 = Node([node1, node2])
+
+    content5 = IntegerContent(0, littleEndian=True)
+    element5 = SizeField(content5, [node3], selfSize=True)
+    
+    element5.notify()
+    assert_equals(element5.compose()       , "\x1e\x00\x00\x00")
+    
+
+def test_SizeField_computeSize():
+    content0 = StringContent("ABCDE")
+    element0 = OpenField(content0, 32)
+
+    content1 = IntegerContent(0)
+    element1 = SizeField(content1, [element0])
+    element1.notify()
+    assert_equals(element1.computeSize()       , 5)
+
+    # Harder example
+    content2 = StringContent("ABCDEFGHIJKLM")
+    element2 = OpenField(content2, 32)
+    content3 = StringContent("12345")
+    element3 = OpenField(content3, 32)
+    content4 = StringContent("#")
+    element4 = PaddingField(content4, 8, [element0])
+    node0 = Node([element2])
+    node1 = Node([node0, element3])
+    node2 = Node([element0, element4])
+    node3 = Node([node1, node2])
+
+    content5 = IntegerContent(0, littleEndian=True)
+    element5 = SizeField(content5, [node3], selfSize=True)
+    
+    element5.notify()
+    assert_equals(element5.computeSize()       , 30)
+
+
+def test_SizeField_notification():
+    """
+        A global test on the notification mechanism.
+    """
+    content0 = StringContent("ABCDE")
+    element0 = OpenField(content0, 32)
+    content1 = IntegerContent(0)
+    element1 = SizeField(content1, [element0])
+    content2 = StringContent("ABCDEFGHIJKLM")
+    element2 = OpenField(content2, 32)
+    content3 = StringContent("12345")
+    element3 = OpenField(content3, 32)
+    content4 = StringContent("#")
+    element4 = PaddingField(content4, 8, [element0])
+    node0 = Node([element2])
+    node1 = Node([node0, element3])
+    node2 = Node([element0, element4])
+    node3 = Node([node1, node2])
+
+    content5 = IntegerContent(0, littleEndian=True)
+    element5 = SizeField(content5, [node3], selfSize=True)
+
+    element0.update("1234567890")
+    element2.update("A")
+
+    assert_equals(element4.compose()            , "######")
+    assert_equals(element5.compose()            , "\x1a\x00\00\x00")
+
+
